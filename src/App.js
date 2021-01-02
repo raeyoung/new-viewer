48ae73be80fed36896d23ecd418020f9b4103135
raeyoung <raeyoung233@gmail.com>
2021-01-02 23:13:35 +0900

    newsapi API 키 발급
    news API key 키 발급하여 적용하기

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ const App = () => {
   const onClick = async () => {
     try {
       const response = await axios.get(
-        'https://jsonplaceholder.typicode.com/todos/1',
+        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=6bd9a952903f42bf9fb3340b231af8e4',
       );
       setData(response.data);
     } catch (e) {
