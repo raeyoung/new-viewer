f1e69e2034b7bd59d2bf05e50ec53c5051eac017
raeyoung <raeyoung233@gmail.com>
2021-01-07 23:16:35 +0900

    API 호출시 카테고리 지정
    현재 카테고리 값이 무엇인지에 따라 동적으로 주소 변경

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -18,7 +18,7 @@ const NewsListBlock = styled.div`
   }
 `;
 
-const NewsList = () => {
+const NewsList = ({ category }) => {
   const [articles, setArticles] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -27,8 +27,9 @@ const NewsList = () => {
     const fetchData = async () => {
       setLoading(true);
       try {
+        const query = category === 'all' ? '' : `&category=${category}`;
         const response = await axios.get(
-          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=6bd9a952903f42bf9fb3340b231af8e4',
+          `https://newsapi.org/v2/top-headlines?country=kr${query}&apiKey=6bd9a952903f42bf9fb3340b231af8e4`,
         );
         setArticles(response.data.articles);
       } catch (e) {
@@ -37,7 +38,7 @@ const NewsList = () => {
       setLoading(false);
     };
     fetchData();
-  }, []);
+  }, [category]);
 
   // 대기 중일 때
   if (loading) {
